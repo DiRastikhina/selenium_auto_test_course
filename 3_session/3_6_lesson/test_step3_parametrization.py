cce61ac35b599f2581de022056cd70fb3aa5c498
DiRastikhina <dianalisheva@gmail.com>
2022-07-18 21:34:01 +0300

    change test 3_6 using conftest.py

--- 3_session/3_6_lesson/test_step3_parametrization.py
+++ 3_session/3_6_lesson/test_step3_parametrization.py
@@ -3,17 +3,9 @@ import time
 import math
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
     
 @pytest.mark.parametrize('link', ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1", "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1", "https://stepik.org/lesson/236899/step/1", "https://stepik.org/lesson/236903/step/1", "https://stepik.org/lesson/236904/step/1", "https://stepik.org/lesson/236905/step/1"])
-def test_guest_should_see_login_link(browser, link):
+def test_should_get_correct_message(browser, link):
     answer = math.log(int(time.time()))
     browser.get(link)
     browser.implicitly_wait(8)
